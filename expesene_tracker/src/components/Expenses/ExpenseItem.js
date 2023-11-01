f63a6d3c2526a3d10797c74451853454274e9d25
PHKhande <phk151raj@gmail.com>
2023-11-01 20:08:32 +0530

    Adding delete expense functionality

--- expesene_tracker/src/components/Expenses/ExpenseItem.js
+++ expesene_tracker/src/components/Expenses/ExpenseItem.js
@@ -4,11 +4,18 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
-const ExpenseItem = ({expense}) => {
+const ExpenseItem = ({expense, setExpense, expenses}) => {
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    setExpense(expenses.filter((ele) => ele.id !== expense.id));
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date = {expense.date}/>
       <ExpenseDetails title={expense.title} location={expense.LocationOfExpenditure} amount={expense.amount}/>
+      <button onClick={clickHandler}> Delete </button>
+
     </Card>
   )
 };
--- expesene_tracker/src/components/Expenses/Expenses.js
+++ expesene_tracker/src/components/Expenses/Expenses.js
@@ -4,12 +4,16 @@ import { expenses } from "../../constants/constants";
 
 import './Expenses.css';
 import Card from "../UI/Card";
+import { useState } from "react";
 const Expenses = () => {
+
+  const [allExpenses, setAllExpenses] = useState(expenses);
+
   return (
     <Card className="expenses">
       <h1>Expense Items</h1>
-      {expenses.map((ele) => {
-        return <ExpenseItem key={ele.id} expense={ele}/>;
+      {allExpenses.map((ele) => {
+        return <ExpenseItem key={ele.id} expense={ele} setExpense={setAllExpenses} expenses={allExpenses}/>;
       })}
     </Card>
   )
