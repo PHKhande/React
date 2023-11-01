4d7156c0f4336254aaad7af1af9a31046b7c1483
PHKhande <phk151raj@gmail.com>
2023-11-01 20:52:51 +0530

    Manipulating the DOM - React States

--- expense_tracker/src/components/Expenses/ExpenseItem.js
+++ expense_tracker/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import Card from "../UI/Card";
 
-const ExpenseItem = ({expense, setExpense, expenses}) => {
+const ExpenseItem = ({ expense, setExpense, expenses }) => {
+  const [title, setTitle] = useState(expense.title);
+  const [amount, setAmount] = useState(expense.amount);
 
-  const clickHandler = (e) => {
-    e.preventDefault();
+  const clickDeleteHandler = () =>
     setExpense(expenses.filter((ele) => ele.id !== expense.id));
-  }
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date = {expense.date}/>
-      <ExpenseDetails title={expense.title} location={expense.LocationOfExpenditure} amount={expense.amount}/>
-      <button onClick={clickHandler}> Delete </button>
+  const clickChangeHandler = () => setTitle("Changed");
+  const clickChangeAmountHandler = () => setAmount("100$");
 
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={expense.date} />
+      <ExpenseDetails
+        title={title}
+        location={expense.LocationOfExpenditure}
+        amount={amount}
+      />
+      <button onClick={clickDeleteHandler}> Delete </button>
+      <button onClick={clickChangeHandler}> Change title </button>
+      <button onClick={clickChangeAmountHandler}> Change amount </button>
     </Card>
-  )
+  );
 };
 
 export default ExpenseItem;
--- expense_tracker/src/components/Expenses/Expenses.js
+++ expense_tracker/src/components/Expenses/Expenses.js
@@ -2,21 +2,27 @@ import ExpenseItem from "./ExpenseItem";
 
 import { expenses } from "../../constants/constants";
 
-import './Expenses.css';
+import "./Expenses.css";
 import Card from "../UI/Card";
 import { useState } from "react";
 const Expenses = () => {
-
   const [allExpenses, setAllExpenses] = useState(expenses);
 
   return (
     <Card className="expenses">
       <h1>Expense Items</h1>
       {allExpenses.map((ele) => {
-        return <ExpenseItem key={ele.id} expense={ele} setExpense={setAllExpenses} expenses={allExpenses}/>;
+        return (
+          <ExpenseItem
+            key={ele.id}
+            expense={ele}
+            setExpense={setAllExpenses}
+            expenses={allExpenses}
+          />
+        );
       })}
     </Card>
-  )
-}
+  );
+};
 
 export default Expenses;
