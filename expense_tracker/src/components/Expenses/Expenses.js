b12b89f8d494cc9e91c52791c53f60c256b83b3d
PHKhande <phk151raj@gmail.com>
2023-11-04 17:32:56 +0530

    Conditional Rendering

--- expense_tracker/src/components/Expenses/Expenses.js
+++ expense_tracker/src/components/Expenses/Expenses.js
@@ -4,28 +4,40 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
 
-const Expenses = ({allExpenses}) => {
-  const [filteredYear, setFilteredYear] = useState('2023');
+const Expenses = ({ allExpenses }) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = allExpenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  console.log(filteredExpenses);
+
+  let expenseContent = <p> No Expense Found. </p>;
+  expenseContent = filteredExpenses.length === 1 && (
+    <>
+      <ExpenseItem key={filteredExpenses[0].id} expense={filteredExpenses[0]} />
+      <p>Only single Expense here. Please add more...</p>
+    </>
+  );
+
+  if (filteredExpenses.length > 1) {
+    expenseContent = filteredExpenses.map((ele) => (
+      <ExpenseItem key={ele.id} expense={ele} />
+    ));
+  }
 
   return (
     <Card className="expenses">
       <h1>Expense Items</h1>
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {allExpenses.map((ele) => {
-        if(ele.date.toString().includes(filteredYear)){
-          return (
-            <ExpenseItem
-              key={ele.id}
-              expense={ele}
-            />
-          );
-
-        } else return null
-      })}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expenseContent}
     </Card>
   );
 };
