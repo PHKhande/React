b9fea22288c8a08b9c892bba17afc3d29cefa09d
PHKhande <phk151raj@gmail.com>
2023-11-04 12:18:42 +0530

    Added non-functional filter

--- expense_tracker/src/components/Expenses/ExpenseItem.js
+++ expense_tracker/src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
-const ExpenseItem = ({ expense, setExpense, expenses }) => {
-  const [title, setTitle] = useState(expense.title);
-  const [amount, setAmount] = useState(expense.amount);
-
-  const clickDeleteHandler = () =>
-    setExpense(expenses.filter((ele) => ele.id !== expense.id));
-  const clickChangeHandler = () => setTitle("Changed");
-  const clickChangeAmountHandler = () => setAmount("100$");
+const ExpenseItem = ({ expense }) => {
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <ExpenseDetails
-        title={title}
+        title={expense.title}
         location={expense.LocationOfExpenditure}
-        amount={amount}
+        amount={expense.amount}
       />
-      <button onClick={clickDeleteHandler}> Delete </button>
-      <button onClick={clickChangeHandler}> Change title </button>
-      <button onClick={clickChangeAmountHandler}> Change amount </button>
     </Card>
   );
 };
--- expense_tracker/src/components/Expenses/Expenses.js
+++ expense_tracker/src/components/Expenses/Expenses.js
@@ -1,19 +1,25 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 const Expenses = ({allExpenses}) => {
+  const [filteredYear, setFilteredYear] = useState('2023');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className="expenses">
       <h1>Expense Items</h1>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {allExpenses.map((ele) => {
         return (
           <ExpenseItem
             key={ele.id}
             expense={ele}
-            // setExpense={setAllExpenses}
-            expenses={allExpenses}
           />
         );
       })}
