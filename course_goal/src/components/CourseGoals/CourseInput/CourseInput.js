80f4e9eeb5b8e4b517b37a733af98c9f66e368d9
PHKhande <phk151raj@gmail.com>
2023-11-27 19:40:04 +0530

    Class based styling

--- course_goal/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course_goal/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,34 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  const [activeAddGoal, setActiveAddGoal] = useState(false);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+      setActiveAddGoal(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      setActiveAddGoal(false); 
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit" style={{backgroundColor: !activeAddGoal ? 'rgb(186, 114, 152)' : '#ac0e77'}}>Add Goal</Button>
     </form>
   );
 };
